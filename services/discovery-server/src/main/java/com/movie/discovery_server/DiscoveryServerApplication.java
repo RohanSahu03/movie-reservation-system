0b76d3d236d68ad26513c70a23def0249683f8fb
Rohan Kumar Sahu <sahurohankumar47@gmail.com>
2026-07-04 01:09:20 +0530

    feat: add actuator and health endpoints to discovery server

--- services/discovery-server/src/main/java/com/movie/discovery_server/DiscoveryServerApplication.java
+++ services/discovery-server/src/main/java/com/movie/discovery_server/DiscoveryServerApplication.java
@@ -1,13 +1,21 @@
 package com.movie.discovery_server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DiscoveryServerApplication.class)
+
 	public static void main(String[] args) {
+		logger.info("Starting Discovery Server...");
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+		logger.info("Discovery Server Started Successfully");
 	}
 
 }

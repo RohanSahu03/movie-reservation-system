55907eadffec089ce2803c520a8a1128698ba053
Rohan Kumar Sahu <sahurohankumar47@gmail.com>
2026-07-16 17:18:33 +0530

    Payment failed Evenet crated

--- services/payment-service/src/main/java/com/movie/payment_service/producer/PaymentEventProducer.java
+++ services/payment-service/src/main/java/com/movie/payment_service/producer/PaymentEventProducer.java
@@ -1,46 +1,46 @@
 package com.movie.payment_service.producer;
 
-
 import com.movie.payment_service.event.PaymentCompletedEvent;
+import com.movie.payment_service.event.PaymentFailedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentEventProducer {
 
+    private static final String PAYMENT_COMPLETED_TOPIC =
+            "payment-completed";
 
-    private static final String TOPIC =
-            "payment-events";
-
+    private static final String PAYMENT_FAILED_TOPIC =
+            "payment-failed";
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-
-
     public void sendPaymentCompletedEvent(
-            PaymentCompletedEvent event
-    ){
+            PaymentCompletedEvent event) {
 
-        log.info(
-                "Publishing payment completed event : {}",
-                event
-        );
-
+        log.info("Publishing PaymentCompletedEvent {}", event);
 
         kafkaTemplate.send(
-                TOPIC,
+                PAYMENT_COMPLETED_TOPIC,
                 event.getBookingId().toString(),
                 event
         );
+    }
 
+    public void sendPaymentFailedEvent(
+            PaymentFailedEvent event) {
 
-        log.info(
-                "Payment event published successfully"
+        log.info("Publishing PaymentFailedEvent {}", event);
+
+        kafkaTemplate.send(
+                PAYMENT_FAILED_TOPIC,
+                event.getBookingId().toString(),
+                event
         );
     }
 
